773f918458744805d03d6035910149ff9b7bfaf0
INO <bih0905@naver.com>
2025-01-27 23:41:06 +0900

    250127

---
+++ meals/lib/screens/categories.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:meals/data/dummy_data.dart';
+import 'package:meals/widgets/category_grid_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 일반적으로 대부분의 앱은 기본적으로 Scaffold를 사용합니다.
+    return Scaffold(
+      // AppBar는 화면 상단에 위치하며, 앱의 이름이나 현재 화면의 제목을 표시합니다.
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      // body는 화면의 주요 콘텐츠를 표시합니다.
+      body: GridView(
+        padding: const EdgeInsets.all(24),
+        // GridView는 그리드 형태의 레이아웃을 표시합니다.
+        gridDelegate:
+            // SliverGridDelegateWithFixedCrossAxisCount는 그리드의 열의 개수를 고정된 값으로 설정합니다.
+            const SliverGridDelegateWithFixedCrossAxisCount(
+          // crossAxisCount는 그리드의 열의 개수를 설정합니다.
+          crossAxisCount: 2,
+          // childAspectRatio는 그리드의 아이템의 가로 세로 비율을 설정합니다.
+          childAspectRatio: 3 / 2,
+          // crossAxisSpacing는 그리드의 열 사이의 간격을 설정합니다.
+          crossAxisSpacing: 20,
+          // mainAxisSpacing는 그리드의 행 사이의 간격을 설정합니다.
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          // avaliableCategories.map((category) => CategoryGridItem(category: category)).toList(),
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+            ),
+        ],
+      ),
+    );
+  }
+}
---
+++ meals/lib/widgets/category_grid_item.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({
+    super.key,
+    required this.category,
+  });
+
+  final Category category;
+
+  @override
+  Widget build(BuildContext context) {
+    // 콘테이너는 일반적으로 배경색 또는 배경 장식을
+    // 설정할 수 있는 많은 옵션을 제공합니다.
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        // !를 정의함으로서 titleLarge가 null이 아님을 보장합니다.
+        style: Theme.of(context).textTheme.titleLarge!.copyWith(
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+      ),
+    );
+  }
+}
