be36f3a982087ddce7e0442bbaef24135c4f7461
INO <bih0905@naver.com>
2025-01-27 22:49:50 +0900

    new project : meal

---
+++ meals/lib/categories.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // 일반적으로 대부분의 앱은 기본적으로 Scaffold를 사용합니다.
+    return Scaffold();
+  }
+}
