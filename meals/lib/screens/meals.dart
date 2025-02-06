be95bc5ceaeaa2ccb24de1563758028a9c05ef11
INO <bih0905@naver.com>
2025-02-06 21:51:59 +0900

    250206_flutter_lecture_168

--- meals/lib/screens/meals.dart
+++ meals/lib/screens/meals.dart
@@ -9,10 +9,12 @@ class MealsScreen extends StatelessWidget {
     super.key,
     this.title,
     required this.meals,
+    required this.onToggleFavorite,
   });
 
   final String? title;
   final List<Meal> meals;
+  final void Function(Meal meal) onToggleFavorite;
 
   // 메뉴 선택 시 처리
   void selectMeal(BuildContext context, Meal meal) {
@@ -20,6 +22,7 @@ class MealsScreen extends StatelessWidget {
       MaterialPageRoute(
         builder: (ctx) => MealDetailsScreen(
           meal: meal,
+          onToggleFavorite: onToggleFavorite,
         ),
       ),
     );
