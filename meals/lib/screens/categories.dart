8941ec743d27f46d1d895d7506d31d649ce1a7cd
INO <bih0905@naver.com>
2025-02-16 13:36:52 +0900

    250216_flutter_lecture_~201

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -6,17 +6,46 @@ import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 // 카테고리 화면
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({
     super.key,
     required this.availableMeals,
   });
 
   final List<Meal> availableMeals;
+
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(microseconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+
+    _animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   // 카테고리가 선택되었을 때의 처리
   void _selectCategory(BuildContext context, Category category) {
     // 매개변수로 받은 카테고리의 id를 이용하여 filteredMeals 리스트를 생성
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where((meal) => meal.categories.contains(category.id))
         .toList();
 
@@ -33,25 +62,40 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      padding: const EdgeInsets.all(24),
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GridView(
+        padding: const EdgeInsets.all(24),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          // avaliableCategories.map((category) => CategoryGridItem(category: category)).toList(),
+          for (final category in availableCategories)
+            CategoryGridItem(
+              category: category,
+              // 카테고리가 선택되었을 때의 처리
+              onSelectedCategory: () {
+                _selectCategory(context, category);
+              },
+            ),
+        ],
       ),
-      children: [
-        // avaliableCategories.map((category) => CategoryGridItem(category: category)).toList(),
-        for (final category in availableCategories)
-          CategoryGridItem(
-            category: category,
-            // 카테고리가 선택되었을 때의 처리
-            onSelectedCategory: () {
-              _selectCategory(context, category);
-            },
+      builder: (context, child) => SlideTransition(
+        position: Tween(
+          begin: const Offset(0, 0.3),
+          end: const Offset(0, 0),
+        ).animate(
+          CurvedAnimation(
+            parent: _animationController,
+            curve: Curves.easeInSine,
           ),
-      ],
+        ),
+        child: child,
+      ),
     );
   }
 }
